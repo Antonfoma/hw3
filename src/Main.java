3d757fda35f73b9688d20752ace16b44d596353b
Anton <prosto-vasay-12@mail.ru>
2022-10-07 18:09:44 +0300

    сделано дз 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" Привет мир !" );
+        System.out.println(" Привет МИР !" );
 
 //    Задание 1
         System.out.println(" Задача 1");
